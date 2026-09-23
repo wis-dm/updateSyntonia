1189ea1fc092dc685a6cadba87c50b1b02417b62
Stephen Wisdom <wisdomstephen03@gmail.com>
2026-09-23 13:22:38 -0700

    Error Handled Cleanly

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 const backToTop = document.querySelector('#backToTop');
@@ -20,27 +19,5 @@ backToTop.addEventListener('click', ()=> {
 //  Hamburger Menu
 hamburger.addEventListener('click', function() {
     navLinks.classList.toggle('show');
-=======
-"use strict";
-
-const backToTop = document.querySelector('#backToTop');
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-
-
-// Back to Top Function
-window.addEventListener('scroll', ()=> {
-    backToTop.classList.toggle('show', window.scrollY > 400);
 });
 
-backToTop.addEventListener('click', ()=> {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-})
-
-
-//  Hamburger Menu
-hamburger.addEventListener('click', function() {
-    navLinks.classList.toggle('show');
->>>>>>> origin/main
-});
